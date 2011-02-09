4e256863a6e46a05b2689175001e0302c727d348
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-02-08 22:06:14 -0600

    coded methods

--- linked-list-lab/List.java
+++ linked-list-lab/List.java
@@ -2,6 +2,7 @@ public class List<E> {
     // Node is accessible inside the class, but not outside.
 
     private class Node {
+
         public E data;
         public Node next;
 
@@ -31,19 +32,19 @@ public class List<E> {
     // Output: int
     public int size() {
         return -1; // an easy bug
+        //return size;
     }
 
     // ----------
     // insert -- place an element at the front of the list
     // Inputs: E data
     // Output: none
-    
     public void insert(E data) {
         first = new Node(data, first);
         size++;
         // This code is also incorrect. inc size
     }
-    
+
     // ----------
     // find -- find an element in the list
     // Inputs: E data
@@ -51,44 +52,59 @@ public class List<E> {
     //         0 = not found
     //         1 = first element, etc.
     public int find(E data) {
-        if (first.data==data) return 1;
+        if (first.data == data) {
+            return 1;
+        }
         Node f = first;
-        int i=1;
-        while (f.next!=null) {
-            if (f.data==data) return i;
-            else i+=1;
-            f=f.next;
+        int i = 1;
+        while (f.next != null) {
+            if (f.data == data) {
+                return i;
+            } else {
+                i += 1;
+            }
+            f = f.next;
         }
         return 0;
     }
-    
+
     // ----------
     // insertAtEnd -- place an element at the end of the list
     // Inputs: E data
     // Output: none
     public void insertAtEnd(E data) {
         Node l = first;
-        while (l.next!=null) {
+        while (l.next != null) {
             l = l.next;
         }
-        l.next = new Node(data,null);
+        l.next = new Node(data, null);
     }
-    
+
     // ----------
     // delete -- delete an element from the list.
     // Inputs: int -- the location of the element to be deleted.
     // Output: none
     public void delete(int index) {
-        for (int i=0;i==index;i++) {
-            
+        Node curr = first;
+        for (int i = 0; i == index; i++) {
+            curr=curr.next;
         }
+        if (curr.next!=null) curr=curr.next.next;
+        else curr=curr.next;
+        size--;
     }
-    
+
     // ----------
     // reverse -- reverse the list
     // Inputs: none
     // Output: none
     public void reverse() {
-        
+        //Node temp = new Node(null,null);
+        Node curr=first;
+        first=new Node(null,null);
+        while (curr.next!=null) {
+            insertAtEnd(curr.data);
+            curr=curr.next;
+        }
     }
 }
--- linked-list-lab/TestList.java
+++ linked-list-lab/TestList.java
@@ -14,12 +14,31 @@ public class TestList extends TestCase {
    }
 
    // test list with nothing in it.
-
    public void testZero() {
       List foo = new List<Integer>();
       assertEquals("Size Test",0,foo.size());
 
       // add more tests here to see if an empty list behaves properly
+   }
+   
+   // test list with values added.
+   public void testFull() {
+       List foo = new List<Integer>();
+       foo.insert(i1);
+       //assertEquals("Insert Test",,);
+       foo.insert(i2);
+       assertEquals("Size Test",2,foo.size());
+       foo.insert(i5);
+       foo.insert(i10);
+       foo.insert(i15);
+       foo.insert(i20);
+       //foo.insert(i25);
+       assertEquals("Find Test",10,foo.find(i10));
+       foo.insert(i25);
+       //assertEquals("insertAtEnd Test",,);
+       foo.delete(4);
+       assertEquals("Delete Test",0,foo.find(i15));
+       //assertEquals("Reverse Test",,);
    }
 
 }
