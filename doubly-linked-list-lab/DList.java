83fcc6a35acc3b1804a8c6d16e45f2837b8d028e
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-03-02 22:45:51 -0600

    a

--- doubly-linked-list-lab/DList.java
+++ doubly-linked-list-lab/DList.java
@@ -1,152 +1,164 @@
 public class DList<E extends Comparable<E>> {
-	
-	private class Node {
-		public E data;
-		public Node next;
-		public Node prev;
-		
-		public Node(E data) {
-			this.data = data;
-		}
-		
-		public Node(Node prev,E data,Node next) {
-			this.data = data;
-		}
-	}
-	
-	private int size;
-	private Node sentinel;
-	
-	public DList() {
-		size = 0;
-		sentinel = null;
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	public void insertFront(E data) {
-		sentinel = new Node(null,data,sentinel);
-		size++;
-	}
-	
-	public void insertEnd(E data) {
-		sentinel = new Node(sentinel,data,null);
-		size++;
-	}
-	
-	public void deleteFront() {
-		sentinel = new Node(sentinel.data);
-		size--;
-	}
-	
-	public void deleteEnd() {
-		sentinel = new Node(sentinel,sentinel.data,sentinel.prev);
-		size--;
-	}
-	
-	private abstract class AllIterator implements Iterator<E> {
-		protected Node cursor;
-		protected boolean valid;
-		
-		public E get() {
-			if (cursor != sentinel)
-				return cursor.data;
-			else
-				return null;
-		}
-		
-		public boolean isValid() {
-			return valid;
-		}
-		
-		public abstract void next();  // set valid to null if next moves to the sentinel
-		
-		public void delete() {
-			if (cursor != sentinel)
-				sentinel.next = cursor.next;
-		}
-	}
-	
-	public class FwdIterator extends AllIterator {
-		public FwdIterator() {
-			cursor = sentinel;
-			this.next();
-		}
-		
-		public void next() {
-			if (cursor != sentinel)
-			cursor = cursor.next;
-		}
-	}
-	
-	public class FwdFindIterator extends AllIterator {
-		E data;
-		boolean found;
-		
-		public FwdFindIterator(E data) {
-			cursor = sentinel;
-			this.data=data;
-			found = false;
-			this.next();
-		}
 
-		public void next() {
-			if (!found) {
-			    if (cursor != sentinel && cursor.data.compareTo(data)!=0)
-				cursor = cursor.next;
-				else if (cursor.data.compareTo(data)==0)
-					found = true;
-			}
-		}
-	}
-	
-	public class RevIterator extends AllIterator {
-		public RevIterator() {
-			cursor = sentinel;
-			this.next();
-		}
-		
-		public void next() {
-			if (cursor != sentinel)
-			cursor = cursor.prev;
-		}
-	}
-	
-	public class RevFindIterator extends AllIterator {
-		E data;
-		boolean found;
-		
-		public RevFindIterator(E data) {
-			cursor = sentinel;
-			found = false;
-			this.next();
-		}
-		
-		public void next() {
-			if (!found) {
-				if (cursor != sentinel && cursor.data.compareTo(data)!=0)
-					cursor = cursor.prev;
-				else if (cursor.data.compareTo(data)==0)
-					found = true;
-			}
-		}
-	}
-	
-	public Iterator makeFwdIterator() {
-		return new FwdIterator();
-	}
-	
-	public Iterator makeRevIterator() {
-		return new RevIterator();
-	}
-	
-	public Iterator makeFwdFindIterator(E data) {
-		return new FwdFindIterator(data);
-	}
-	
-	public Iterator makeRevFindIterator(E data) {
-		return new RevFindIterator(data);
-	}
+    private class Node {
+
+        public E data;
+        public Node next;
+        public Node prev;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public Node(Node prev, E data, Node next) {
+            this.data = data;
+        }
+    }
+    private int size;
+    private Node sentinel;
+
+    public DList() {
+        size = 0;
+        sentinel = null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void insertFront(E data) {
+        sentinel = new Node(null, data, sentinel);
+        size++;
+    }
+
+    public void insertEnd(E data) {
+        sentinel = new Node(sentinel, data, null);
+        size++;
+    }
+
+    public void deleteFront() {
+        sentinel = new Node(sentinel.data);
+        size--;
+    }
+
+    public void deleteEnd() {
+        sentinel = new Node(sentinel, sentinel.data, sentinel.prev);
+        size--;
+    }
+
+    private abstract class AllIterator implements Iterator<E> {
+
+        protected Node cursor;
+        protected boolean valid;
+
+        public E get() {
+            if (cursor != sentinel) {
+                return cursor.data;
+            } else {
+                return null;
+            }
+        }
+
+        public boolean isValid() {
+            return valid;
+        }
+
+        public abstract void next();  // set valid to null if next moves to the sentinel
+
+        public void delete() {
+            if (cursor != sentinel) {
+                sentinel.next = cursor.next;
+            }
+        }
+    }
+
+    public class FwdIterator extends AllIterator {
+
+        public FwdIterator() {
+            cursor = sentinel;
+            this.next();
+        }
+
+        public void next() {
+            if (cursor != sentinel) {
+                cursor = cursor.next;
+            }
+        }
+    }
+
+    public class FwdFindIterator extends AllIterator {
+
+        E data;
+        boolean found;
+
+        public FwdFindIterator(E data) {
+            cursor = sentinel;
+            this.data = data;
+            found = false;
+            this.next();
+        }
+
+        public void next() {
+            if (!found) {
+                if (cursor != sentinel && data.compareTo(cursor.data) != 0) {
+                    cursor = cursor.next;
+                } else if (data.compareTo(cursor.data) == 0) {
+                    found = true;
+                }
+            }
+        }
+    }
+
+    public class RevIterator extends AllIterator {
+
+        public RevIterator() {
+            cursor = sentinel;
+            this.next();
+        }
+
+        public void next() {
+            if (cursor != sentinel) {
+                cursor = cursor.prev;
+            }
+        }
+    }
+
+    public class RevFindIterator extends AllIterator {
+
+        E data;
+        boolean found;
+
+        public RevFindIterator(E data) {
+            cursor = sentinel;
+            this.data = data;
+            found = false;
+            this.next();
+        }
+
+        public void next() {
+            if (!found) {
+                if (cursor != sentinel && data.compareTo(cursor.data) != 0) {
+                    cursor = cursor.prev;
+                } else if (data.compareTo(cursor.data) == 0) {
+                    found = true;
+                }
+            }
+        }
+    }
+
+    public Iterator makeFwdIterator() {
+        return new FwdIterator();
+    }
+
+    public Iterator makeRevIterator() {
+        return new RevIterator();
+    }
+
+    public Iterator makeFwdFindIterator(E data) {
+        return new FwdFindIterator(data);
+    }
+
+    public Iterator makeRevFindIterator(E data) {
+        return new RevFindIterator(data);
+    }
 }
--- doubly-linked-list-lab/TestDList.java
+++ doubly-linked-list-lab/TestDList.java
@@ -1,81 +1,81 @@
 import junit.framework.*;
 
 public class TestDList extends TestCase {
-	public Integer i1 = new Integer(1);
-	public Integer i2 = new Integer(2);
-	public Integer i5 = new Integer(5);
-	public Integer i10 = new Integer(10);
-	public Integer i15 = new Integer(15);
-	public Integer i20 = new Integer(20);
-	public Integer i25 = new Integer(25);
-	
-	public TestDList(String name) {
-		super(name);
-	}
-	
-	public void testCreate() {
-		DList<Integer> d = new DList<Integer>();
-		
-		assertEquals("1. create",0,d.size());
-		d.insertFront(i1);
-		d.deleteEnd();
-		assertEquals("size doesn't decrement on deletion",0,d.size());
-	}
-	
-	public void testFrontInsertion() {
-		DList<Integer> d = new DList<Integer>();
-		d.insertFront(i1);
-		d.insertFront(i2);
-		d.insertFront(i5);
-		Iterator fwd = d.makeFwdIterator();
-		Iterator rev = d.makeRevIterator();
-		assertEquals("insertFront 3 items",3,d.size());
-		assertEquals("front item correct",i5,fwd.get());
-		assertEquals("last item correct",i1,rev.get());
-		d.deleteFront();
-		assertEquals("deleteFront decrements size",2,d.size());
-		d.insertFront(i5);
-		assertEquals("forward iterator starts at first element",i5,fwd.get());
-		fwd.next();
-		assertEquals("forward iterator only returns first element",i2,fwd.get());
-		
-		assertEquals("reverse iterator is really a forward iterator",false,fwd.equals(rev));
-		assertEquals("reverse iterator only returns last element",i1,rev.get());
-		rev.next();
-		assertEquals("reverse iterator only returns last element",i2,rev.get());
-	}
-	
-	public void testEndInsertion() {
-		DList<Integer> d = new DList<Integer>();
-		d.insertEnd(i1);
-		d.insertEnd(i2);
-		d.insertEnd(i5);
-		Iterator fwd = d.makeFwdIterator();
-		Iterator rev = d.makeRevIterator();
-		assertEquals("insertEnd 3 items",3,d.size());
-		assertEquals("front item correct",i1,fwd.get());
-		assertEquals("last item correct",i5,rev.get());
-		d.deleteEnd();
-		assertEquals("deleteEnd decrements size",2,d.size());
-		d.insertEnd(i5);
-		
-	}
-	
-	public void testFind() {
-		DList<Integer> d = new DList<Integer>();
-		d.insertEnd(i1);
-		d.insertEnd(i2);
-		d.insertEnd(i5);
-		Iterator fwdfind = d.makeFwdFindIterator(i1);
-		Iterator revfind = d.makeRevFindIterator(i5);
-		assertEquals("element found fwd",i1,fwdfind.get());
-		assertEquals("element found rev",i5,revfind.get());
-		fwdfind.next();
-		revfind.next();
-		fwdfind.next();
-		revfind.next();
-		assertEquals("fwdfind returns everything",i1,fwdfind.get());
-		assertEquals("revfind returns everything",i5,revfind.get());
-	}
-	
+
+    public Integer i1 = new Integer(1);
+    public Integer i2 = new Integer(2);
+    public Integer i5 = new Integer(5);
+    public Integer i10 = new Integer(10);
+    public Integer i15 = new Integer(15);
+    public Integer i20 = new Integer(20);
+    public Integer i25 = new Integer(25);
+
+    public TestDList(String name) {
+        super(name);
+    }
+
+    public void testCreate() {
+        DList<Integer> d = new DList<Integer>();
+
+        assertEquals("1. create", 0, d.size());
+        d.insertFront(i1);
+        d.deleteEnd();
+        assertEquals("size doesn't decrement on deletion", 0, d.size());
+    }
+
+    public void testFrontInsertion() {
+        DList<Integer> d = new DList<Integer>();
+        d.insertFront(i1);
+        d.insertFront(i2);
+        d.insertFront(i5);
+        Iterator fwd = d.makeFwdIterator();
+        Iterator rev = d.makeRevIterator();
+        assertEquals("insertFront 3 items", 3, d.size());
+        assertEquals("front item correct", i5, fwd.get());
+        assertEquals("last item correct", i1, rev.get());
+        d.deleteFront();
+        assertEquals("deleteFront decrements size", 2, d.size());
+        d.insertFront(i5);
+        assertEquals("forward iterator starts at first element", i5, fwd.get());
+        fwd.next();
+        assertEquals("forward iterator only returns first element", i2, fwd.get());
+
+        assertEquals("reverse iterator is really a forward iterator", false, fwd.equals(rev));
+        assertEquals("reverse iterator only returns last element", i1, rev.get());
+        rev.next();
+        assertEquals("reverse iterator only returns last element", i2, rev.get());
+    }
+
+    public void testEndInsertion() {
+        DList<Integer> d = new DList<Integer>();
+        d.insertEnd(i1);
+        d.insertEnd(i2);
+        d.insertEnd(i5);
+        Iterator fwd = d.makeFwdIterator();
+        Iterator rev = d.makeRevIterator();
+        assertEquals("insertEnd 3 items", 3, d.size());
+        assertEquals("front item correct", i1, fwd.get());
+        assertEquals("last item correct", i5, rev.get());
+        d.deleteEnd();
+        assertEquals("deleteEnd decrements size", 2, d.size());
+        d.insertEnd(i5);
+
+    }
+
+    public void testFind() {
+        DList<Integer> d = new DList<Integer>();
+        d.insertEnd(i1);
+        d.insertEnd(i2);
+        d.insertEnd(i5);
+        Iterator fwdfind = d.makeFwdFindIterator(i1);
+        Iterator revfind = d.makeRevFindIterator(i5);
+        assertEquals("element found fwd", i1, fwdfind.get());
+        assertEquals("element found rev", i5, revfind.get());
+        fwdfind.next();
+        revfind.next();
+        fwdfind.next();
+        revfind.next();
+        assertEquals("fwdfind returns everything", i1, fwdfind.get());
+        assertEquals("revfind returns everything", i5, revfind.get());
+    }
 }
