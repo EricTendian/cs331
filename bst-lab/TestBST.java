16e7b1c97463be9498ffe83873d0869db89cd3b7
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-03-20 16:43:27 -0500

    fix

--- bst-lab/TestBST.java
+++ bst-lab/TestBST.java
@@ -68,13 +68,13 @@ public class TestBST extends TestCase {
         bst.add(i5,n5);
         Iterator iter = bst.mkBFSIterator();
         iter.next();
-        assertEquals("root is correct", i1, iter.get());
+        assertEquals("root is i2", i2, iter.get());
         iter.next();
-        assertEquals("next item is i2", i2, iter.get());
-        iter.next();
-        assertEquals("next item is i3", i3, iter.get());
+        assertEquals("next item is i1", i1, iter.get());
         iter.next();
         assertEquals("next item is i4", i4, iter.get());
+        iter.next();
+        assertEquals("next item is i3", i3, iter.get());
         iter.next();
         assertEquals("next item is i5", i5, iter.get());
         iter.next();
