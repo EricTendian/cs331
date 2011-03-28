7d4ae07ef8da4b41ce739ef30dc8b83fe12d37c3
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-03-28 14:41:41 -0500

    a

---
+++ heap-lab/TestHeap.java
@@ -0,0 +1,12 @@
+import junit.framework.*;
+
+public class TestHeap extends TestCase {
+
+    public TestHeap(String name) {
+        super(name);
+    }
+
+    public void testEmpty() {
+        Heap heap = new Heap();
+    }
+}
