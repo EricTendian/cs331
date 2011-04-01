d297604f1cc62a884d7df5c2be84be60cb184141
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-04-01 16:17:41 -0500

    a

--- heap-lab/TestHeap.java
+++ heap-lab/TestHeap.java
@@ -21,23 +21,23 @@ public class TestHeap extends TestCase {
         heap.enqueue(1);
         assertEquals("correct item", 1, heap.top());
         heap.enqueue(3);
-        assertEquals("correct item", 3, heap.top());
+        assertEquals("correct item", 1, heap.top());
         heap.enqueue(6);
-        assertEquals("correct item", 3, heap.top());
+        assertEquals("correct item", 1, heap.top());
         heap.enqueue(5);
-        assertEquals("correct item", 3, heap.top());
+        assertEquals("correct item", 1, heap.top());
         heap.enqueue(9);
-        assertEquals("correct item", 3, heap.top());
+        assertEquals("correct item", 1, heap.top());
         heap.enqueue(8);
-        assertEquals("correct item", 3, heap.top());
+        assertEquals("correct item", 1, heap.top());
         assertEquals("size is incremented", 6, heap.size());
         
+        assertEquals("correct element dequeued", 1, heap.dequeue());
+        assertEquals("correct element dequeued", 8, heap.dequeue());
+        assertEquals("correct element dequeued", 9, heap.dequeue());
+        assertEquals("correct element dequeued", 5, heap.dequeue());
+        assertEquals("correct element dequeued", 6, heap.dequeue());
         assertEquals("correct element dequeued", 3, heap.dequeue());
-        assertEquals("correct element dequeued", 3, heap.dequeue());
-        assertEquals("correct element dequeued", 1, heap.dequeue());
-        assertEquals("correct element dequeued", 1, heap.dequeue());
-        assertEquals("correct element dequeued", 1, heap.dequeue());
-        assertEquals("correct element dequeued", 1, heap.dequeue());
         assertEquals("size is decremented", 6, heap.size());
     }
 }
