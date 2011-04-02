60fff440767cfa08a5f2d739c7085e6943d7a6fe
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-04-01 22:44:57 -0500

    a

--- heap-lab/Heap.java
+++ heap-lab/Heap.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Heap<E extends Comparable> {
     private int size;
@@ -23,17 +24,11 @@ public class Heap<E extends Comparable> {
         return (i - 1) / 2;
     }
     
-    private void swap(int pos1, int pos2) {
-        E tmp = data.get(pos1);
-        data.add(pos1, data.get(pos2));
-	data.add(pos2, tmp);
-    }
-    
     private void heapifyUp(int index) {
         if (index!=0) {
             int parent = parent(index);
 	    if (data.get(parent).compareTo(data.get(index))>0) {
-                swap(index, parent);
+                Collections.swap(data, index, parent);
                 heapifyUp(parent);
 	    }
         }
@@ -42,17 +37,11 @@ public class Heap<E extends Comparable> {
     private void heapifyDown(int index) {
         int left = left(index);
         int right = right(index);
-        int min;
-        if (right>=size) {
-            if (left>=size) return;
-            else min = left;
-        } else {
-            if (data.get(left).compareTo(data.get(right))<=0) min = left;
-            else min = right;
-        } if (data.get(index).compareTo(data.get(min))<0) {
-            swap(index, min);
-            heapifyDown(min);
-        }
+        if (right>size-1) if (left>size-1) return;
+        if (data.get(left).compareTo(data.get(right))>0) ++left;
+        if (data.get(index).compareTo(data.get(left))<=0) return;
+        Collections.swap(data, index, left);
+        heapifyDown(left);
     }
 
     // Main functions
@@ -63,8 +52,8 @@ public class Heap<E extends Comparable> {
 
     public void enqueue(E elt) {
         if (size>0) {
-            data.add(size-1, elt);
-            heapifyUp(size-1);
+            data.add(size, elt);
+            heapifyUp(size);
         } else data.add(0, elt);
         size++;
     }
@@ -72,9 +61,8 @@ public class Heap<E extends Comparable> {
     public E dequeue() {
         if (size>0) {
             E del = data.get(0);
-            data.add(0, data.get(size-1));
-            size--;
-            if (size>0) heapifyDown(0);
+            data.add(0, data.get(--size));
+            if (size>1) heapifyDown(0);
             return del;
 	} else return null;
     }
