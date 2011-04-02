5d877510ea77b69fa28e383ece8b413b6c817f7b
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-04-01 23:16:07 -0500

    a

--- heap-lab/Heap.java
+++ heap-lab/Heap.java
@@ -13,35 +13,37 @@ public class Heap<E extends Comparable> {
     // Helper functions
 
     private int left(int i) {
-        return 2 * i + 1;
+        return 2*i;
     }
 
     private int right(int i) {
-        return 2 * i + 2;
+        return 2*i+1;
     }
 
     private int parent(int i) {
-        return (i - 1) / 2;
+        return i/2;
     }
     
-    private void heapifyUp(int index) {
+    private void upHeap(int index) {
         if (index!=0) {
             int parent = parent(index);
 	    if (data.get(parent).compareTo(data.get(index))>0) {
                 Collections.swap(data, index, parent);
-                heapifyUp(parent);
+                upHeap(parent);
 	    }
         }
     }
     
-    private void heapifyDown(int index) {
+    private void downHeap(int index) {
         int left = left(index);
         int right = right(index);
-        if (right>size-1) if (left>size-1) return;
-        if (data.get(left).compareTo(data.get(right))>0) ++left;
-        if (data.get(index).compareTo(data.get(left))>=0) return;
-        Collections.swap(data, index, left);
-        heapifyDown(left);
+        int min = index;
+        if (left<=size-1 && data.get(left).compareTo(data.get(index))<0) min=left;
+        if (right<=size-1 && data.get(right).compareTo(data.get(min))>=0) min=right;
+        if (min!=index) {
+            Collections.swap(data, index, left);
+            downHeap(min);
+        }
     }
 
     // Main functions
@@ -53,7 +55,7 @@ public class Heap<E extends Comparable> {
     public void enqueue(E elt) {
         if (size>0) {
             data.add(size, elt);
-            heapifyUp(size);
+            upHeap(size-1);
         } else data.add(0, elt);
         size++;
     }
@@ -61,8 +63,8 @@ public class Heap<E extends Comparable> {
     public E dequeue() {
         if (size>0) {
             E del = data.get(0);
-            data.add(0, data.get(--size));
-            if (size>1) heapifyDown(0);
+            data.add(0, data.remove(--size));
+            if (size>1) downHeap(0);
             return del;
 	} else return null;
     }
