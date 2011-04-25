33985076eb344daf1b19daad033ab2aa2e8b18dd
Eric R. Tendian <etendian@dijkstra.cs.iit.edu>
2011-04-25 15:12:53 -0500

    a

--- up-tree-lab/TestUpTree.java
+++ up-tree-lab/TestUpTree.java
@@ -58,12 +58,12 @@ public class TestUpTree extends TestCase {
         up.add("str4");
         up.union(str3, str4);
         Iterator<UpTree.Node> itr = up.mkSetIterator(str2);
-        //assertEquals("setiteratortest1", true, str1.equals(itr.get()));
+        assertEquals("setiteratortest1", true, str4.equals(itr.get()));
         itr.next();
-        //assertEquals("setiteratortest2", true, str2.equals(itr.get()));
+        assertEquals("setiteratortest2", true, str2.equals(itr.get()));
         itr.next();
-        //assertEquals("setiteratortest3", true, str3.equals(itr.get()));
+        assertEquals("setiteratortest3", true, str3.equals(itr.get()));
         itr.next();
-        //assertEquals("setiteratortest4", true, str4.equals(itr.get()));
+        assertEquals("setiteratortest4", true, str1.equals(itr.get()));
     }
 }
